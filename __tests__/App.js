f9baf0e9242d611939dd0f7977610352d1b87391
ewonguoft <eltong.wong@mail.utoronto.ca>
2018-01-29 17:31:51 -0500

    fixing circleci build

---
+++ __tests__/App.js
@@ -0,0 +1,9 @@
+import 'react-native';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('basic test', () => {
+    expect(2 + 2).toBe(4);
+
+});
